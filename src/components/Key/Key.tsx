bfe0f5e1c1c6afa202ead07719514868b55617ba
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-06 10:20:32 -0600

    connect keyboard with instruments

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -3,17 +3,32 @@ import { NoteType } from "../../domain/note";
 import clsx from "clsx";
 import styles from "./Key.module.css";
 
+type PressCallback = () => void;
+
 type KeyPros = {
   type: NoteType;
   label: string;
   disabled?: boolean;
+
+  onUp: PressCallback;
+  onDown: PressCallback;
 };
 
-export const Key: FunctionComponent<KeyPros> = (pros) => {
-  const { type, label, ...rest } = pros;
-
+export const Key: FunctionComponent<KeyPros> = ({
+  type,
+  label,
+  onDown,
+  onUp,
+  ...rest
+}) => {
   return (
-    <button className={clsx(styles.key, styles[type])} type="button" {...rest}>
+    <button
+      className={clsx(styles.key, styles[type])}
+      onMouseDown={onDown}
+      onMouseUp={onUp}
+      type="button"
+      {...rest}
+    >
       {label}
     </button>
   );
