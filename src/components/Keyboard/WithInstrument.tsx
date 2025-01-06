d406ee11d0446ec93355d250d7650bfd0662c21d
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-06 11:51:27 -0600

    make it work with different instruments

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,13 +1,17 @@
+import { useEffect } from "react";
 import { useSoundfont } from "../../adapters/Soundfont";
-import { useMount } from "../../utils/useMount";
+import { useInstrument } from "../../state/Instrument";
 import { useAudioContext } from "../AudioContextProvider";
 import { Keyboard } from "./Keyboard";
 
 export const KeyboardWithInstrument = () => {
   const AudioContext = useAudioContext()!;
-  const { loading, play, stop, load } = useSoundfont({ AudioContext });
+  const { instrument } = useInstrument();
+  const { loading, current, play, stop, load } = useSoundfont({ AudioContext });
 
-  useMount(() => load());
+  useEffect(() => {
+    if (!loading && instrument !== current) load(instrument);
+  }, [load, loading, current, instrument]);
 
   return <Keyboard loading={loading} play={play} stop={stop} />;
 };
