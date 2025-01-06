31d0e8b911890bc030c287d9a0b09c2e8e7115ff
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-06 09:37:33 -0600

    create sound provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Logo } from "./components/Logo";
 import { Footer } from "./components/Footer";
 import styles from "./App.module.css";
+import { Main } from "./components/Main";
 
 export const App = () => {
   return (
     <div className={styles.app}>
       <Logo />
-      <main className={styles.content} />
+      <main className={styles.content}>
+        <Main />
+      </main>
       <Footer />
     </div>
   );
