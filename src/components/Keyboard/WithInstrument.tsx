912bd98ca86d2ed544a52fab59565401887f026a
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-07 11:03:38 -0600

    use HOC pattern

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,18 +1,25 @@
-import { SoundfontProvider } from "../../adapters/Soundfont";
+// import { SoundfontProvider } from "../../adapters/Soundfont";
 // import { SoundfontProvider } from "../../adapters/Soundfont/SoundfontProviderClass";
+import { withInstrument } from "../../adapters/Soundfont";
 import { useInstrument } from "../../state/Instrument";
 import { useAudioContext } from "../AudioContextProvider";
 import { Keyboard } from "./Keyboard";
+
+const WrappedKeyboard = withInstrument(Keyboard);
 
 export const KeyboardWithInstrument = () => {
   const AudioContext = useAudioContext()!;
   const { instrument } = useInstrument();
 
+  // return (
+  //   <SoundfontProvider
+  //     AudioContext={AudioContext}
+  //     instrument={instrument}
+  //     render={(props) => <Keyboard {...props} />}
+  //   />
+  // );
+
   return (
-    <SoundfontProvider
-      AudioContext={AudioContext}
-      instrument={instrument}
-      render={(props) => <Keyboard {...props} />}
-    />
+    <WrappedKeyboard AudioContext={AudioContext} instrument={instrument} />
   );
 };
